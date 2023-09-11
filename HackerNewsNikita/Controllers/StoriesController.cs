8822c9ea04326e363af786055fbd2eade6d7a2f6
NikitaPatil312 <patilnikita.1296@gmail.com>
2023-09-11 13:24:46 +0530

    Upload Data
    commit

---
+++ HackerNewsNikita/Controllers/StoriesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using HackerNewsNikita.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HackerNewsNikita.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoriesController : ControllerBase
+    {
+        
+        private readonly IConfigurationRoot _config;
+
+        public StoriesController(IConfiguration config)
+        {
+            _config = (IConfigurationRoot)config;
+        }
+
+        [HttpGet]
+        [Route("{noOfStories}")]
+        public async Task<ActionResult> GetBestStories([FromRoute]int noOfStories)
+        {
+            return await CallHackNewsAPI(noOfStories);
+        }
+
+        private async Task<ActionResult> CallHackNewsAPI(int noOfStories)
+        {
+            try
+            {
+                List<StoryModel> stories = new List<StoryModel>();
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = _config["APIEndpoints:BestStoriesAPI"]!;
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+
+                        return NoContent();
+                    }
+                    var resultContent = await response.Content.ReadFromJsonAsync<List<int>>();
+                    if (noOfStories > resultContent?.Count)
+                    {
+                        return NotFound(new JsonResult("Stories ask count is bigger than total count of best stories"));
+                    }
+                    var filteredList = resultContent!.Take(noOfStories);
+
+                    foreach (var singleRecord in filteredList)
+                    {
+                        string apiForStoryByID = _config["APIEndpoints:StoryFromID"]!;
+                        HttpResponseMessage bestStoryData = await client.GetAsync(string.Format(apiForStoryByID, singleRecord));
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            continue;
+                        }
+                        var storyData = JsonConvert.DeserializeObject<StoryModel>(bestStoryData.Content.ReadAsStringAsync().Result);
+                        stories.Add(storyData!);
+
+                    }
+                    
+                    return new JsonResult(stories.OrderBy(x=>x.score)); 
+                }
+
+                 
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
+
---
+++ HackerNewsNikita/Models/StoryModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace HackerNewsNikita.Models
+{
+
+    public class StoryModel
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+
+        [JsonProperty("by")]
+        public string postedBy { get; set; }
+
+        private string _time;
+        public string time { get { return _time; } set {
+                _time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(value)).ToUniversalTime().ToString("o");
+
+            } }
+        public int score { get; set; }
+        [JsonProperty("descendants")]
+        public int commentCount { get; set; }
+
+
+    }
+}
+
